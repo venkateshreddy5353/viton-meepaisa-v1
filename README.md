504661359d7ce35d561deaa67baa2ca914eb2b84
Cyrus Tsiu <cyrustsiu@gmail.com>
2022-04-13 22:05:19 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,118 @@
+# Vitrual Try-On *Flask*
+
+[中文文档📚](http://github.com/GrayXu/Virtual-Try-On-Flask/blob/master/README_cn.md)
+
+![20200118004320.png](https://raw.githubusercontent.com/GrayXu/Online-Storage/master/img/20200118004320.png)![20200118004342.png](https://raw.githubusercontent.com/GrayXu/Online-Storage/master/img/20200118004342.png)![20200118004359.png](https://raw.githubusercontent.com/GrayXu/Online-Storage/master/img/20200118004359.png)
+
+A multi-stage virtual try-on deep neural networks based on [JPP-Net and CP-VTON](#References). We provide **SIMPLE and EASY-to-handle API** on upper level, and combine some traditional image processing methods.
+
+Feature:
+- Fast **Human_Image + Cloth_Image = Gen_Image**, put on a specified upper clothes for specified people.
+- Web and Backend response service on *Flask*
+
+# How to use
+
+## Run complete server codes
+
+1. Download 3 [pretrained models](#checkpoints) to `/checkpoints`.
+2. install [dependency packages](#Dependency)
+3. Start Flask service `python main.py`
+
+Clients encode image in base64, and send them in [Json format](http://github.com/GrayXu/Virtual-Try-On-Flask/blob/master/main.py#L141). Then server would return results in the same rule.
+
+## Run only network codes
+
+`Model.py` is the only file that needs attention. After install dependency and initialize `model.init(path...)`, you can directly call `model.predict(human_img, c_img, ...)` to make prediction. All image are numpy arrays on RGB channels.
+
+[Template notebook](http://github.com/GrayXu/Virtual-Try-On-Flask/blob/master/Template.ipynb) would be a nice example to show you how to use it.
+
+Model's core external function is ["predict()"](http://github.com/GrayXu/Virtual-Try-On-Flask/blob/master/Model.py#L36). Just pay attention to 5 flag parameters in documentation comments.
+
+```
+def predict(self, human_img, c_img, need_pre=True, need_bright=False, keep_back=False, need_dilate=False, check_dirty=False):
+    '''
+    parameters: 
+        human_img: human's image
+        c_img: cloth image with the shape of (256,192,3) RGB
+        need_pre: need preprocessing, including crop and zoom 
+        need_bright: brightness enhancement
+        keep_back: keep image's background
+        need_dilate: if keep_back is True, and you need dilate the mask
+        check_dirty: check limb cross
+    return: 
+        a (256, 192, 3) image with specified people wearing specified clothes
+        confidence of this image (a float)
+    '''
+```
+
+# Dependency
+
+Recommended python version==3.6
+`pip install -r requirements.txt`
+
+~~*tensorflow==1.12.0  
+torch==1.2.0
+torchvision==0.2.0*~~  
+
+~~*For now, GPU environment is essential. If you want to run it on CPU environment, delete all ` ` calls in CPVTON.py and Networks.py.*~~   
+CPU Mode: `Model(.., use_cuda = False)`
+
+# Hardware Usage
+
+On Tesla P40, GPU memory usage is about 7.12Gb. Each prediction costs about 0.62s (JPPNet 0.6s, CPVTON 0.02s).
+
+*If you want Real-Time speed, try to change the method of human feature extraction, like CE2P.*
+
+# Network Architecture
+
+This multi-stage network consists of 3 parts:
+- **JPP-Net**: Extract human features, make pose estimation & human parsing
+- **Geometric Matching Module**: Input human features and clothes images, and twist clothes based on learned thin-plate-spline algorithm.
+- **Try-on Module**: Input human feature and twisted clothes images, and generate try-on images.
+
+
+![20200117182844.png](https://raw.githubusercontent.com/GrayXu/Online-Storage/master/img/20200117182844.png)
+
+# File
+
+Fname | Usage  
+-|-  
+main.py | Flask service  
+get.py | clients post predict requests to Flask server  
+Model.py | Virtual Try-on Net
+CPVTON.py | CPVTON model (GMM+TOM)
+networks.py | CPVTON's basic network unit
+JPPNet.py | JPP-Net model's init & predict
+static/* | static resource
+data/\*, example/\* | example test images
+template/\* | Flask's html file based on Jinjia
+checkpoints/\* | checkpoints dir
+
+# Checkpoints
+
+~~*coming soon..*~~  
+download link on [*Google Drive*](https://drive.google.com/file/d/125UtOS4T4RBji8lXtm9WEwD1KcHG4F1g/view?usp=sharing) and [*Baidu Pan*](https://pan.baidu.com/s/1e8tKEz7hpHAxqV6B5_hOIw)
+
+
+# TODO List
+
+- [x] Optimize model  
+- [x] Web try-on service  
+- [x] Basic documentation and comments  
+- [x] Client post documentation  
+- [x] Faster models download support  
+- [x] CPU support
+
+# References
+
+Model designs are based on JPP-Net & CPVTON, and their open-source repo on Github.  
+
+[**VITON**: An Image-based Virtual Try-on Network](https://arxiv.org/abs/1711.08447v1),Xintong Han, Zuxuan Wu, Zhe Wu, Ruichi Yu, Larry S. Davis. **CVPR 2018**
+
+[(**CP-VTON**) Toward Characteristic-Preserving Image-based Virtual Try-On Networks](https://arxiv.org/abs/1807.07688), Bochao Wang, Huabin Zheng, Xiaodan Liang, Yimin Chen, Liang Lin, Meng Yang. **ECCV 2018**
+
+[(**JPP-Net**) Look into Person: Joint Body Parsing & Pose Estimation Network and A New Benchmark](https://arxiv.org/abs/1804.01984), Xiaodan Liang, Ke Gong, Xiaohui Shen, Liang Lin, **T-PAMI 2018**.
+
+----
+
+*Powered By **Imba**, [JD Digits](https://www.jddglobal.com/)*
---
+++ main.py
@@ -0,0 +1,228 @@
+'''
+本文件负责VTO的flask web服务端
+提供web服务、响应客户端预测请求
+This file implements the web server on flask.
+It can provide web service, response all predict requests.
+'''
+import os
+from flask import Flask, render_template, request, url_for, send_from_directory
+from flask_debugtoolbar import DebugToolbarExtension
+from PIL import Image
+import numpy as np
+import time
+import platform
+from Model import Model
+import random
+import cv2
+from io import StringIO, BytesIO
+import base64
+from datetime import datetime
+import json
+import logging
+
+# Use base64 to send & receive images between clients and the server
+def readb64(base64_string):
+    sbuf = BytesIO()
+    sbuf.write(base64.b64decode(base64_string))
+    res = Image.open(sbuf)
+    return np.array(res)
+
+
+def writeb64(img):
+    img_str = cv2.imencode('.bmp', img)[1]
+    imagebase64 = base64.b64encode(img_str)
+    imagebase64 = bytes.decode(imagebase64)
+    return imagebase64
+
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+# init for all global variables
+
+model = Model("checkpoints/jpp.pb",
+              "checkpoints/gmm.pth",
+              "checkpoints/tom.pth",
+               use_cuda=False)
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = 'development key'
+toolbar = DebugToolbarExtension(app)
+
+# UPLOAD_FOLDER = 'request_upload'
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# model image list for web server
+person_list_raw = os.listdir(os.path.join(BASE_DIR, "static/web_option", "person"))
+person_list = []
+p_counter = 0
+for person in person_list_raw:
+    if 'jpg' in person:
+        person_list.append([os.path.join("./static/web_option","person",person), p_counter])
+        p_counter += 1
+
+# cloth list for web server
+cloth_list_raw = os.listdir(os.path.join(BASE_DIR, "static/web_option", "cloth"))
+cloth_list = []
+c_counter = 0
+for cloth in cloth_list_raw:
+    if 'jpg' in cloth:
+        cloth_list.append([os.path.join("./static/web_option", "cloth", cloth), c_counter])
+        c_counter += 1
+
+# Use "/web" url to get web page
+@app.route('/web')
+def hello_world():
+    return render_template('login.html', person_list=person_list, img_list=cloth_list)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_image():
+    print(request.form)
+    print(request.files)
+    if (not len(request.files) == 2 or (len(request.form) == 1 and len(request.files) == 1)):
+        return render_template('login.html', info="selection error", person_list=person_list, img_list=cloth_list)
+    else:
+        c_index = 0  # init
+        p_index = 0 # init
+        cloth_image = None
+        person_image = None
+        
+        # 4 combinations of uploading and selecting model or cloth images 
+        if 'p_optionsRadios' in request.form:
+            p_index = int(request.form['p_optionsRadios'][6:])
+            person_image = person_list[p_index][0]
+            if 'c_optionsRadios' in request.form:
+                c_index = int(request.form['c_optionsRadios'][6:])
+            else:
+                cloth_image = request.files['cloth_image']
+        
+        if 'p_optionsRadios' not in request.form:
+            person_image = request.files['person_image']
+            if 'c_optionsRadios' in request.form:
+                c_index = int(request.form['c_optionsRadios'][6:])
+            else:
+                cloth_image = request.files['cloth_image']
+        
+        print(f"You are going to try on {cloth_image} with {person_image} as model...")
+        start_time = time.time()
+        o_name, h_name = run_model_web(
+            person_image, cloth_list[c_index][0].split("\\")[-1], cloth_image)
+        end_time = time.time()
+        if o_name is None: # bad cloth image
+            return 'I told you only clothes image with shape 256*192*3'
+        else:
+            return render_template(
+                'login.html', 
+                person_list=person_list,
+                img_list=cloth_list,
+                result1=h_name,
+                result2=o_name,
+                info="time: %.3f" % (end_time-start_time))
+
+
+def run_model_web(f, cloth_name, cloth_f=None):
+    '''
+    为web服务进行预测。cloth_name和cloth_f中必有一个有内容，优先选择cloth_f，即用户上传的衣服图片
+    prediction service. cloth_name and cloth_f cannot be both None. cloth_f is prior, which is from user upload.
+    '''
+    if cloth_f is None:
+        print(f, cloth_name)
+        c_img = np.array(Image.open(os.path.join("static/web_option","cloth",cloth_name)))
+    else:
+        print(f, cloth_f)
+        try:
+            c_img = np.array(Image.open(cloth_f))
+        except:
+            c_img = np.array(Image.open(os.path.join("static/web_option","cloth",cloth_name)))
+
+    # 固化到本地的缓存文件夹，访问的时候作为静态资源被调用
+    # local resource temp file would be used as static resource.
+    temp_o_name = os.path.join("static", "result", "%d_%s" % (
+        int(time.time()), cloth_name.split("/")[-1]))
+    temp_h_name = os.path.join("static", "human", "%d_%s" % (
+        int(time.time()), cloth_name.split("/")[-1]))
+
+    if c_img.shape[0] != 256 or c_img.shape[1] != 192 or c_img.shape[2] != 3:
+        return None, None
+
+    img = Image.open(f)
+    human_img = np.array(img)
+
+    out, v = model.predict(human_img, c_img, need_bright=False, keep_back=True)
+    print("v:"+str(v))
+    out = np.array(out, dtype='uint8')
+
+    img.save(temp_h_name)
+    Image.fromarray(out).save(temp_o_name, quality=95)  # 注意这个95
+    return temp_o_name, temp_h_name
+
+
+def getimg():
+    data_str = request.data
+    data_str = bytes.decode(data_str)
+    data_str = data_str.replace('\n', '')
+    data_json = json.loads(data_str)
+    base64img_p = data_json['image_person']
+    img_person = readb64(base64img_p)
+    img_person = cv2.rotate(img_person, 2)
+    img_person = cv2.flip(img_person, 1)
+    base64img_c = data_json['image_cloth']
+    img_cloth = readb64(base64img_c)
+    return [img_person, img_cloth]
+
+
+'''
+json format example:
+client:
+    {
+        'image_person':'...',
+        'image_cloth':'...'
+    }
+
+server:
+    {
+        'status':'ok',
+        'output_image':'...'
+    }
+'''
+@app.route('/cloth', methods=['GET', 'POST'])
+def Hello_cloth():
+    '''
+    响应客户端请求
+    reponse requests from clients
+    '''
+    output_str = ""
+    output_json = {}
+    status = 'ok'
+    if request.method == 'POST':
+        # temp file would be writed to root dir
+        input_person, input_cloth = getimg()
+        cv2.imwrite('in.jpg', input_person)
+        input_person = input_person[60:580, 45:435]
+        cv2.imwrite('in_2.jpg', input_person)
+        output_img, v = model.predict(input_person, input_cloth, need_bright=True, keep_back=True, need_dilate=True)
+        output_img = cv2.cvtColor(output_img, cv2.COLOR_RGB2BGR)
+        cv2.imwrite('out.jpg', output_img)
+        print("v:"+str(v))
+        output_base64 = writeb64(output_img)
+        if v < 0.1: # confidence is too weak to show
+            status = 'failure'
+        else:
+            status = 'ok'
+        output_json["status"] = status
+        output_json["output_image"] = output_base64
+        output_str = json.dumps(output_json)
+        return output_str
+    return "please use http client to request!"
+
+
+if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+    # run server locally
+    app.run(port=5000)
+
+    # or as a servers
+    # app.run(host='0.0.0.0', port=5000)
